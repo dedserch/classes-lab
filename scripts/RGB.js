b02ca06947fad38d5ac5d6e96c9a0f40b4dd0d69
Dmitry Serzhputovski <123190027+dedserch@users.noreply.github.com>
2024-03-28 19:03:49 +0300

    add: more method in RGB

--- scripts/RGB.js
+++ scripts/RGB.js
@@ -1,22 +1,25 @@
 class RGB {
-    #red
-    #green
-    #blue
-    constructor(red,green,blue){
-        this.#validateColor(red)
-        this.#validateColor(green)
-        this.#validateColor(blue)
+    #red;
+    #green;
+    #blue;
 
-        this.#red = red
-        this.#green = green
-        this.#blue = blue
+    constructor(red, green, blue) {
+        this.setColors(parseInt(red), parseInt(green), parseInt(blue))
     }
+
     #validateColor = (value) => {
         if (isNaN(value) || value < 0 || value > 255) {
-          throw new Error("Некорректное значение цвета: " + value);
+            throw new Error("Некорректное значение цвета: " + value)
         }
     }
-    setColors = (red,green,blue) => {
+
+    #validateObject = (otherColor) => {
+        if (!(otherColor instanceof RGB)) {
+            throw new Error("Некорректный тип объекта: " + otherColor)
+        }
+    }
+
+    setColors = (red, green, blue) => {
         this.#validateColor(red)
         this.#validateColor(green)
         this.#validateColor(blue)
@@ -24,12 +27,56 @@ class RGB {
         this.#red = red
         this.#green = green
         this.#blue = blue
+
+        return this
     }
+
+    add = (otherColor) => {
+        this.#validateObject(otherColor)
+
+        const newRed = this.#red + otherColor.#red
+        const newGreen = this.#green + otherColor.#green
+        const newBlue = this.#blue + otherColor.#blue
+    
+        return new RGB(newRed, newGreen, newBlue)
+    }
+    
+    subtract = (otherColor) => {
+        this.#validateObject(otherColor)
+        
+        const newRed = this.#red - otherColor.#red
+        const newGreen = this.#green - otherColor.#green
+        const newBlue = this.#blue - otherColor.#blue
+    
+        return new RGB(newRed, newGreen, newBlue)
+    }
+    multiply = (factor) => {
+        const newRed = Math.min(Math.round(this.#red * factor), 255)
+        const newGreen = Math.min(Math.round(this.#green * factor), 255)
+        const newBlue = Math.min(Math.round(this.#blue * factor), 255)
+
+        return new RGB(newRed, newGreen, newBlue)
+    }
+    
     isSimilarTo = (otherColor) => {
+        this.#validateObject(otherColor)
+
         const deltaRed = Math.abs(this.#red - otherColor.#red)
         const deltaGreen = Math.abs(this.#green - otherColor.#green)
         const deltaBlue = Math.abs(this.#blue - otherColor.#blue)
-
+    
         return deltaRed <= 10 && deltaGreen <= 10 && deltaBlue <= 10
+    }
+    
+    set = (otherColor) => {
+        this.#validateObject(otherColor)
+
+        this.#red = otherColor.#red
+        this.#green = otherColor.#green
+        this.#blue = otherColor.#blue
+    }
+
+    toString = () => {
+        return `RGB(${this.#red}, ${this.#green}, ${this.#blue})`
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,32 @@
-const colorTest = new RGB(20,50,100)
-console.log(colorTest); // Проверка на инициализацию класса
+const colorTest = new RGB (100,200,30)
+console.log(colorTest); // проверка на инициализацию объекта(работает)
 
-colorTest.setColors(100,200,50)
-console.log(colorTest); // Проверка на изменение свойств
+//////////////////////////////////////////////////////////////////////
+const color1 = new RGB (20,20,20)
+const color2 = new RGB ("20","20","20")
 
-//colorTest.setColors(1000,"5000",-1)
-//console.log(colorTest); //Проверка на ошибку при неправильном вводе данных. При этих данных нам выбьет: "Некорректное значение цвета: " + value
+console.log(color1.add(color2)); 
+// здесь у нас создается новый объект с новыми значениями. 
+//Т.к мы решили, что "ВСЕ математические операции должны возвращать НОВЫЙ объект, а не менять текущий."
+//Это значит, если мы пропишем console.log(color1) Мы заметим, что color1 останется с параметрами (20,20,20)
+//теперь проверим операцию присваивания, но для начала поменяем значение цвета у color2 на(40,40,40)
+color2.setColors(40,40,40)
+color1.set(color2)
+console.log(color1) // теперь мы заметили, что color1 присвоил значение color2. Метод работает
 
-const color1 = new RGB(255, 0, 0)
-const color2 = new RGB(250, 10, 10)
+console.log(color1.isSimilarTo(color2)) // возращает true т.к цвета похожи
 
-console.log(color1.isSimilarTo(color2)); // выведет true
+console.log(color1.toString()) // выводит информацию про RGB в виде строки
 
-color1.setColors(10,20,50)
-console.log(color1.isSimilarTo(color2)); // выведет false
+console.log(color1.multiply(2))
+//////////////////////////////////////////////////////////////////////
+//Проверка ошибок (просто уберите комментарий )
+//const colorTest2 = new RGB (-2,10,11)
+//const colorTest2 = new RGB ("ad20",20,"100")
+//const colorTest2 = new RGB ("100","20","30")
+//colorTest2.add(2)
+
+
 
 
 
